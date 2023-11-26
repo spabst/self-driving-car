06b1ff5598922718432f346f910dd99af0b61bdc
Sebastian Pabst <sebastian.pabst@LumRisk.com>
2023-11-26 15:29:48 +0100

    added sensors

--- car.js
+++ car.js
@@ -12,11 +12,13 @@ class Car{
 
         this.angle=0;
 
+        this.sensor = new Sensor(this)
         this.controls= new Controls()
     }
 
     update(){
         this.#move();
+        this.sensor.update();
     }
 
     #move(){
@@ -70,6 +72,8 @@ class Car{
         ctx.fill();
 
         ctx.restore()
+
+        this.sensor.draw(ctx)
 
     }
 }
