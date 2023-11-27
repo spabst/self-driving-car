7accf912a41cd4c7a1336d59c5b8344924e0a43c
Sebastian Pabst <sebastian.pabst@LumRisk.com>
2023-11-27 13:46:05 +0100

    replace car rect with a polygon for damage detection

--- car.js
+++ car.js
@@ -18,7 +18,32 @@ class Car{
 
     update(roadBorders){
         this.#move();
+        this.polygon= this.#createPolygon()
         this.sensor.update(roadBorders);
+    }
+
+    #createPolygon(){
+        const points=[];
+        const radius=Math.hypot(this.width, this.height)/2;
+        const alpha= Math.atan2(this.width, this.height);
+        // top-right angle
+        points.push({
+            x: this.x-Math.sin(this.angle-alpha)*radius,
+            y: this.y-Math.cos(this.angle-alpha)*radius,
+        });
+        points.push({
+            x: this.x-Math.sin(this.angle+alpha)*radius,
+            y: this.y-Math.cos(this.angle+alpha)*radius,
+        });
+        points.push({
+            x: this.x-Math.sin(Math.PI+this.angle-alpha)*radius,
+            y: this.y-Math.cos(Math.PI+this.angle-alpha)*radius,
+        });
+        points.push({
+            x: this.x-Math.sin(Math.PI+this.angle+alpha)*radius,
+            y: this.y-Math.cos(Math.PI+this.angle+alpha)*radius,
+        });
+        return points;
     }
 
     #move(){
@@ -59,19 +84,12 @@ class Car{
     }
 
     draw(ctx){
-        ctx.save();
-        ctx.translate(this.x, this.y);
-        ctx.rotate(-this.angle);
         ctx.beginPath();
-        ctx.rect(
-            -this.width/2,
-            -this.height/2,
-            this.width,
-            this.height
-        );
+        ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
+        this.polygon.slice(1).map(point=>{
+            ctx.lineTo(point.x, point.y);
+        })
         ctx.fill();
-
-        ctx.restore()
 
         this.sensor.draw(ctx)
 
