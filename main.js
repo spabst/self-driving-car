f4ce5c532f798b5bf2a20b5c13c5627ec99d43fe
Sebastian Pabst <sebastian.pabst@LumRisk.com>
2023-11-28 22:16:52 +0100

    added the neural network and attached it ro a car

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-const canvas = document.getElementById("myCanvas");
-canvas.width=200;
+const carCanvas = document.getElementById("carCanvas");
+carCanvas.width=200;
+const networkCanvas = document.getElementById("networkCanvas");
+networkCanvas.width=300;
 
-const ctx= canvas.getContext("2d");
-const road= new Road(canvas.width/2, canvas.width*0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS", maxSpeed=4);
+const carCtx= carCanvas.getContext("2d");
+const networkCtx= networkCanvas.getContext("2d");
+
+
+const road= new Road(carCanvas.width/2, carCanvas.width*0.9);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI", maxSpeed=4);
 
 const traffic=[
     new Car(road.getLaneCenter(1), -100, 30,50, "DUMMY", 2)
@@ -11,20 +16,24 @@ const traffic=[
 
 animate();
 
-function animate(){
+function animate(time){
 
     traffic.map(c=>{
         c.update(road.borders,[]);
     })
     car.update(road.borders, traffic);
-    canvas.height=window.innerHeight;
+    carCanvas.height=window.innerHeight;
+    networkCanvas.height=window.innerHeight;
 
-    ctx.save();
-    ctx.translate(0, -car.y + canvas.height*0.75);
+    carCtx.save();
+    carCtx.translate(0, -car.y + carCanvas.height*0.75);
 
-    road.draw(ctx);
-    traffic.map(c=>{c.draw(ctx, "purple");})
-    car.draw(ctx, "blue");
-    ctx.restore();
+    road.draw(carCtx);
+    traffic.map(c=>{c.draw(carCtx, "purple");})
+    car.draw(carCtx, "blue");
+    carCtx.restore();
+
+    networkCtx.lineDashOffset= time/50;
+    Visualizer.drawNetwork(networkCtx, car.brain);
     requestAnimationFrame(animate);
 }
